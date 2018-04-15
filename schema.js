e11cc9e8707425a2446bebb5610e1f68b27a498c
Jean Carlo <jeancarlo.flores@vasslatam.com>
2018-04-15 11:19:28 -0500

    Creando schema

--- schema.js
+++ schema.js
@@ -3,13 +3,40 @@ const { makeExecutableSchema } = require('graphql-tools')
 //El schema debe tener un querytype donde definimos al cual le estamos dando
 //acceso a nuestros clientes
 const typeDefs = `
+    #Clase de Curso
     type Curso {
         id: ID!
         titulo: String!
+        #Descripcion del curso
+        descripcion: String!
+        profesor: Profesor
+        rating: Float
+        comentario: [Comentario]
+    }
+    type Profesor {
+        id: ID!
+        nombre: String!
+        nacionalidad: String!
+        genero: Genero
+        cursos: [Curso]
+    }
+
+    enum Genero {
+        MASCULINO
+        FEMENINO
+    }
+
+    type Comentario {
+        id: ID!
+        nombre: String!
+        cuerpo: String!
     }
 
     type Query {
         cursos: [Curso]
+        profesores: [Profesor]
+        curso(id: Int):Curso
+        profesor(id: Int): Profesor
     }
 `
 
