adaca2f2e35b21cba3a9506900d4bcdb3a8e3bef
Jean Carlo <jeancarlo.flores@vasslatam.com>
2018-04-15 11:06:26 -0500

    set up proyect

---
+++ schema.js
@@ -0,0 +1,19 @@
+const { makeExecutableSchema } = require('graphql-tools')
+//El schema debe tener un querytype donde definimos al cual le estamos dando
+//acceso a nuestros clientes
+const typeDefs = `
+    type Curso {
+        id: ID!
+        titulo: String!
+    }
+
+    type Query {
+        cursos: [Curso]
+    }
+`
+
+const schema = makeExecutableSchema({
+    typeDefs
+})
+
+module.exports = schema
